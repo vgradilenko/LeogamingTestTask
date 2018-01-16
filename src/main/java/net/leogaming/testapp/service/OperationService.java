3d365196189105226c61649a66305af9cc7ea237
vgradilenko <gradilenko@gmail.com>
2018-01-17 01:47:37 +0200

    fixed methods verify-payment-status. TODO: add logger to class

--- src/main/java/net/leogaming/testapp/service/OperationService.java
+++ src/main/java/net/leogaming/testapp/service/OperationService.java
@@ -5,19 +5,21 @@ import net.leogaming.testapp.entity.request.Request;
 import net.leogaming.testapp.entity.request.StatusEntity;
 import net.leogaming.testapp.entity.request.VerifyEntity;
 
-import net.leogaming.testapp.entity.response.AbstractResponseBody;
-import net.leogaming.testapp.entity.response.Response;
 import net.leogaming.testapp.entity.response.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Component;
 
+import javax.xml.bind.JAXBException;
+import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Component
 public class OperationService {
+    private static Logger logger = Logger.getLogger(OperationService.class.getName());
     private final RequestHelper requestHelper;
 
     @Autowired
@@ -25,30 +27,21 @@ public class OperationService {
         this.requestHelper = requestHelper;
     }
 
-    public ResultEntity verify(int point, VerifyEntity verifyEntity) {
+    public ResultEntity verify(int point, VerifyEntity verifyEntity) throws JAXBException, SignatureException {
         Request request = new Request(point, Collections.singletonList(verifyEntity));
-        Response response = requestHelper.makeRequest(request);
-        return (ResultEntity) response.getResponseBody().get(0);
+        return  requestHelper.executeRequest(request).getResponseBody().get(0);
     }
 
-    public List<ResultEntity> payment(int point, List<PaymentEntity> paymentEntityList) {
+    public List<ResultEntity> payment(int point, List<PaymentEntity> paymentEntityList) throws JAXBException, SignatureException {
         Request request = new Request(point, paymentEntityList);
-        List<AbstractResponseBody> responseBodies = requestHelper.makeRequest(request).getResponseBody();
-        return getResultEntity(responseBodies);
+        List<ResultEntity> responseBodies = requestHelper.executeRequest(request).getResponseBody();
+        return responseBodies;
     }
 
-    public List<ResultEntity> status(int point, List<StatusEntity> statusEntityList) {
+    public List<ResultEntity> status(int point, List<StatusEntity> statusEntityList) throws JAXBException, SignatureException {
         Request request = new Request(point, statusEntityList);
-        List<AbstractResponseBody> responses = requestHelper.makeRequest(request).getResponseBody();
-        return getResultEntity(responses);
-    }
-
-    private List<ResultEntity> getResultEntity(List<AbstractResponseBody> abstractResponseBodies) {
-        List<ResultEntity> resultEntities = new ArrayList<>();
-        for(AbstractResponseBody body: abstractResponseBodies) {
-            resultEntities.add((ResultEntity)body);
-        }
-
-        return resultEntities;
+        List<ResultEntity> responses = requestHelper.executeRequest(request).getResponseBody();
+        return responses;
     }
 }
+
