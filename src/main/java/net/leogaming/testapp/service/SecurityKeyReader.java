0480375e35895b5399ccd1e02927a96b0573e9e6
vgradilenko <gradilenko@gmail.com>
2018-01-17 01:45:11 +0200

    added new logic to get security keys: classLoader load keys from resources

--- src/main/java/net/leogaming/testapp/service/SecurityKeyReader.java
+++ src/main/java/net/leogaming/testapp/service/SecurityKeyReader.java
@@ -3,23 +3,28 @@ package net.leogaming.testapp.service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Objects;
 
 @Component
 public class SecurityKeyReader {
 
-    private final static String PATH_TO_PUBLIC_Key = "C:\\point_327\\public_key.der";
-    private final static String PATH_TO_PRIVATE_KEY = "C:\\point_327\\private_key.der";
+    private final static String PUBLIC_KEY = "public_key.der";
+    private final static String PRIVATE_KEY = "private_key.der";
+    private ClassLoader classLoader = getClass().getClassLoader();
 
     @Bean
     public PublicKey getPublicKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get(PATH_TO_PUBLIC_Key));
+        File file = new File(Objects.requireNonNull(classLoader.getResource(PUBLIC_KEY)).getFile());
+        byte[] keyBytes = Files.readAllBytes(file.toPath());
         X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePublic(spec);
@@ -27,10 +32,10 @@ public class SecurityKeyReader {
 
     @Bean
     public PrivateKey getPrivateKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get(PATH_TO_PRIVATE_KEY));
+        File file = new File(Objects.requireNonNull(classLoader.getResource(PRIVATE_KEY)).getFile());
+        byte[] keyBytes = Files.readAllBytes(file.toPath());
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePrivate(spec);
     }
 }
-//https://stackoverflow.com/questions/11410770/load-rsa-public-key-from-file
